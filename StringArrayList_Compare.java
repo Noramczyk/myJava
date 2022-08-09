9a6b10b796cf60c2b6ba47638f9ed6b69e4a2a6c
Noramczyk <58186854+Noramczyk@users.noreply.github.com>
2022-08-09 08:38:40 -0700

    Create StringArrayList_Compare.java

---
+++ StringArrayList_Compare.java
@@ -0,0 +1,17 @@
+String[] products = new String[]{"Keyboard","Mouse","Samsung","Ram"};
+String[] temp = new String[]{"Keyboard", "Cat", "Beaver", "Ram"};
+
+ArrayList<String> l1 = new ArrayList<String>();
+ArrayList<String> l2 = new ArrayList<String>();
+
+for(String elements: products)
+{
+  l1.add(elements1)
+  }
+for(String elements2: temp)
+{
+  l2.add(elements2)
+  }
+
+l2.removeAll(l1); //remove difference
+
